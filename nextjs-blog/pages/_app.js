2d74a96d5229a52a29c57bda488146176bb034cc
MWest2020 <mwesterweel@hotmail.com>
2021-08-02 00:07:28 +0200

    trying proptypes

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
+import PropTypes from 'react';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {Component, pageProps} />;
+}
+
+MyApp.propTypes ={
+  Component: PropTypes.element,
+  pageProps: PropTypes.element
 }
