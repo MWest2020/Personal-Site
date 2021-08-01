64b9fb6365f43c17a9590574a236fb8bc33563e2
MWest2020 <mwesterweel@hotmail.com>
2021-08-01 23:47:40 +0200

    value

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,6 +1,5 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {Component, pageProps} />;
 }
