1ae086102f58e673cd38060b484d50a0fc73f2a4
MWest2020 <mwesterweel@hotmail.com>
2021-08-02 02:11:35 +0200

    profile

--- nextjs-blog/components/Layout.js
+++ nextjs-blog/components/Layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import profile from 'images/profile.jpg'
+import profile from '../public/images/profile.jpg'
 
 const name = 'Mark Westerweel';
 export const siteTitle = 'Personal Page of Mark';
