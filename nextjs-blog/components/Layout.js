6b1d49a8207bfb2047834ecec01dc337d123fa13
MWest2020 <mwesterweel@hotmail.com>
2021-08-02 02:01:07 +0200

    profile

--- nextjs-blog/components/Layout.js
+++ nextjs-blog/components/Layout.js
@@ -30,10 +30,10 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/public/images/profile.jpg"
+              src="images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={400}
+              width={400}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/public/images/profile.jpg"
+                  src="images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
